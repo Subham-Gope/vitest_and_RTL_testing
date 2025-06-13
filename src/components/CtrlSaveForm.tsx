1a145feafb94a6837fe49213c27e67ef0d3ca6f1
Subham Gope <subhamgopebusiness@gmail.com>
2025-06-13 10:22:38 +0530

    project updated

--- src/components/CtrlSaveForm.tsx
+++ src/components/CtrlSaveForm.tsx
@@ -1,1 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import type { KeyboardEvent } from "react";
+
+const CtrlSaveForm: React.FC = () => {
+  const [text, setText] = useState<string>("");
+
+  return (
+    <>
+      <label htmlFor="ctrl-s-input">Enter Your Text</label>
+      <input id="ctrl-s-input" type="text" placeholder="Example Text" />
+      
+    </>
+  );
+};
+
+export default CtrlSaveForm;
